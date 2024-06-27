9cd23507c94dde1be30550c8a698e124e6b4f531
Mariano Capiliku <marianocapiliku@gmail.com>
2024-06-27 01:40:52 +0100

    Getting user session information

--- app/_components/Resevation.jsx
+++ app/_components/Resevation.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReservationForm from "./ReservationForm";
 import DateSelector from "./DateSelector";
+import LoginMessage from "../_components/LoginMessage";
 import { getBookedDatesByCabinId, getSettings } from "../_libs/data-service";
+import { auth } from "../_libs/auth";
 
 async function Resevation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+
+  const session = await auth();
 
   return (
     <div className=" grid grid-cols-2 border border-primary-800 min-h-[400px] mt-8">
@@ -16,7 +20,11 @@ async function Resevation({ cabin }) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm user={session.user} cabin={cabin} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
